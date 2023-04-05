d1674721358580df38b34c41c4078a2aa841b7a4
Mariusz <84902763+zubryckim@users.noreply.github.com>
2023-04-05 10:26:02 +0200

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,13 +1,7 @@
-<?php    
-//$config['host']="serwer2012768.home.pl";
-//$config['database']="33017218_rkinvest";
-//$config['user']="33017218_rkinvest";
-//$config['password']="Mariano121!";
-
 $config['host']="localhost";
 $config['database']="rkinvestment";
 $config['user']="rkinvest_db";
-$config['password']="90uimFG_jh34GT";
+$config['password']="????????";
 
 $conn1 = new PDO("mysql:host={$config['host']};dbname={$config['database']};", $config['user'], $config['password'], [
 		PDO::ATTR_EMULATE_PREPARES => false, 
