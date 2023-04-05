227af9da2549a5153b60adaec81d222e87437540
Mariusz <84902763+zubryckim@users.noreply.github.com>
2023-04-05 10:26:59 +0200

    Update connectmysqli.php

--- config/connectmysqli.php
+++ config/connectmysqli.php
@@ -1,14 +1,9 @@
 <?php    
-/*
-$config['host']="serwer2012768.home.pl";
-$config['database']="33017218_rkinvest";
-$config['user']="33017218_rkinvest";
-$config['password']="Mariano121!";
-*/
+
 $config['host']="localhost";
 $config['database']="rkinvestment";
 $config['user']="rkinvest_db";
-$config['password']="90uimFG_jh34GT";
+$config['password']="???????????";
 
 
 $conn1 = mysqli_connect($config['host'], $config['user'], $config['password'], $config['database']);
